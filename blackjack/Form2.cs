98202b58b1b80162b97135e5e96bd2523690dbfd
Jopolle <46864640+Jopolle@users.noreply.github.com>
2025-04-04 16:17:04 +0200

    add stand and dealer plays. TODO: win condition, stand if bet present

---
+++ blackjack/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace blackjack
+{
+    public partial class Form2 : Form
+    {
+        public Form2(Form1 f)
+        {
+            InitializeComponent();
+            rodzic = f;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rodzic.rozdaj();
+            this.Close();
+        }
+    }
+}
--- blackjack/obj/Debug/net8.0-windows/blackjack.AssemblyInfo.cs
+++ blackjack/obj/Debug/net8.0-windows/blackjack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c6cd30654cfe4b6d4513e3e96c6dfec34680d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478e6b4ad0fbbb9096090d5eafbd89d265577067")]
 [assembly: System.Reflection.AssemblyProductAttribute("blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
