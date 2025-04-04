00282ba4084d64d80afaa3037e5057e7e93e5f00
Jopolle <46864640+Jopolle@users.noreply.github.com>
2025-04-04 12:06:44 +0200

    Poprawa

--- blackjack/Form1.Designer.cs
+++ blackjack/Form1.Designer.cs
@@ -69,9 +69,10 @@
             // 
             // hit_button
             // 
-            hit_button.Location = new Point(99, 463);
+            hit_button.Location = new Point(113, 617);
+            hit_button.Margin = new Padding(3, 4, 3, 4);
             hit_button.Name = "hit_button";
-            hit_button.Size = new Size(102, 111);
+            hit_button.Size = new Size(117, 148);
             hit_button.TabIndex = 0;
             hit_button.Text = "Hit";
             hit_button.UseVisualStyleBackColor = true;
@@ -79,9 +80,10 @@
             // 
             // stand_button
             // 
-            stand_button.Location = new Point(224, 463);
+            stand_button.Location = new Point(256, 617);
+            stand_button.Margin = new Padding(3, 4, 3, 4);
             stand_button.Name = "stand_button";
-            stand_button.Size = new Size(102, 111);
+            stand_button.Size = new Size(117, 148);
             stand_button.TabIndex = 1;
             stand_button.Text = "Stand";
             stand_button.UseVisualStyleBackColor = true;
@@ -89,25 +91,26 @@
             // label_value
             // 
             label_value.AutoSize = true;
-            label_value.Location = new Point(99, 399);
+            label_value.Location = new Point(113, 532);
             label_value.Name = "label_value";
-            label_value.Size = new Size(67, 15);
+            label_value.Size = new Size(85, 20);
             label_value.TabIndex = 2;
             label_value.Text = "Hand Value";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(415, 508);
+            textBox1.Location = new Point(474, 677);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(114, 27);
             textBox1.TabIndex = 3;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(359, 511);
+            label1.Location = new Point(410, 681);
             label1.Name = "label1";
-            label1.Size = new Size(50, 15);
+            label1.Size = new Size(65, 20);
             label1.TabIndex = 4;
             label1.Text = "Bet Size:";
             // 
@@ -115,9 +118,11 @@
             // 
             dealer1.BackColor = Color.Transparent;
             dealer1.BackgroundImageLayout = ImageLayout.None;
-            dealer1.Location = new Point(81, 52);
+            dealer1.Image = Properties.Resources._10_of_clubs;
+            dealer1.Location = new Point(93, 69);
+            dealer1.Margin = new Padding(3, 4, 3, 4);
             dealer1.Name = "dealer1";
-            dealer1.Size = new Size(105, 147);
+            dealer1.Size = new Size(120, 196);
             dealer1.SizeMode = PictureBoxSizeMode.StretchImage;
             dealer1.TabIndex = 5;
             dealer1.TabStop = false;
@@ -127,9 +132,10 @@
             // 
             dealer2.BackColor = Color.Transparent;
             dealer2.BackgroundImageLayout = ImageLayout.None;
-            dealer2.Location = new Point(192, 52);
+            dealer2.Location = new Point(219, 69);
+            dealer2.Margin = new Padding(3, 4, 3, 4);
             dealer2.Name = "dealer2";
-            dealer2.Size = new Size(105, 147);
+            dealer2.Size = new Size(120, 196);
             dealer2.SizeMode = PictureBoxSizeMode.StretchImage;
             dealer2.TabIndex = 6;
             dealer2.TabStop = false;
@@ -138,9 +144,10 @@
             // 
             dealer3.BackColor = Color.Transparent;
             dealer3.BackgroundImageLayout = ImageLayout.None;
-            dealer3.Location = new Point(303, 52);
+            dealer3.Location = new Point(346, 69);
+            dealer3.Margin = new Padding(3, 4, 3, 4);
             dealer3.Name = "dealer3";
-            dealer3.Size = new Size(105, 147);
+            dealer3.Size = new Size(120, 196);
             dealer3.SizeMode = PictureBoxSizeMode.StretchImage;
             dealer3.TabIndex = 7;
             dealer3.TabStop = false;
@@ -149,9 +156,10 @@
             // 
             dealer4.BackColor = Color.Transparent;
             dealer4.BackgroundImageLayout = ImageLayout.None;
-            dealer4.Location = new Point(415, 52);
+            dealer4.Location = new Point(474, 69);
+            dealer4.Margin = new Padding(3, 4, 3, 4);
             dealer4.Name = "dealer4";
-            dealer4.Size = new Size(105, 147);
+            dealer4.Size = new Size(120, 196);
             dealer4.SizeMode = PictureBoxSizeMode.StretchImage;
             dealer4.TabIndex = 8;
             dealer4.TabStop = false;
@@ -160,9 +168,10 @@
             // 
             player4.BackColor = Color.Transparent;
             player4.BackgroundImageLayout = ImageLayout.None;
-            player4.Location = new Point(415, 238);
+            player4.Location = new Point(474, 317);
+            player4.Margin = new Padding(3, 4, 3, 4);
             player4.Name = "player4";
-            player4.Size = new Size(105, 147);
+            player4.Size = new Size(120, 196);
             player4.SizeMode = PictureBoxSizeMode.StretchImage;
             player4.TabIndex = 12;
             player4.TabStop = false;
@@ -171,9 +180,10 @@
             // 
             player3.BackColor = Color.Transparent;
             player3.BackgroundImageLayout = ImageLayout.None;
-            player3.Location = new Point(303, 238);
+            player3.Location = new Point(346, 317);
+            player3.Margin = new Padding(3, 4, 3, 4);
             player3.Name = "player3";
-            player3.Size = new Size(105, 147);
+            player3.Size = new Size(120, 196);
             player3.SizeMode = PictureBoxSizeMode.StretchImage;
             player3.TabIndex = 11;
             player3.TabStop = false;
@@ -182,9 +192,10 @@
             // 
             player2.BackColor = Color.Transparent;
             player2.BackgroundImageLayout = ImageLayout.None;
-            player2.Location = new Point(192, 238);
+            player2.Location = new Point(219, 317);
+            player2.Margin = new Padding(3, 4, 3, 4);
             player2.Name = "player2";
-            player2.Size = new Size(105, 147);
+            player2.Size = new Size(120, 196);
             player2.SizeMode = PictureBoxSizeMode.StretchImage;
             player2.TabIndex = 10;
             player2.TabStop = false;
@@ -194,9 +205,10 @@
             // 
             player1.BackColor = Color.Transparent;
             player1.BackgroundImageLayout = ImageLayout.None;
-            player1.Location = new Point(81, 238);
+            player1.Location = new Point(93, 317);
+            player1.Margin = new Padding(3, 4, 3, 4);
             player1.Name = "player1";
-            player1.Size = new Size(105, 147);
+            player1.Size = new Size(120, 196);
             player1.SizeMode = PictureBoxSizeMode.StretchImage;
             player1.TabIndex = 9;
             player1.TabStop = false;
@@ -205,9 +217,10 @@
             // 
             player5.BackColor = Color.Transparent;
             player5.BackgroundImageLayout = ImageLayout.None;
-            player5.Location = new Point(526, 238);
+            player5.Location = new Point(601, 317);
+            player5.Margin = new Padding(3, 4, 3, 4);
             player5.Name = "player5";
-            player5.Size = new Size(105, 147);
+            player5.Size = new Size(120, 196);
             player5.SizeMode = PictureBoxSizeMode.StretchImage;
             player5.TabIndex = 14;
             player5.TabStop = false;
@@ -216,9 +229,10 @@
             // 
             dealer5.BackColor = Color.Transparent;
             dealer5.BackgroundImageLayout = ImageLayout.None;
-            dealer5.Location = new Point(526, 52);
+            dealer5.Location = new Point(601, 69);
+            dealer5.Margin = new Padding(3, 4, 3, 4);
             dealer5.Name = "dealer5";
-            dealer5.Size = new Size(105, 147);
+            dealer5.Size = new Size(120, 196);
             dealer5.SizeMode = PictureBoxSizeMode.StretchImage;
             dealer5.TabIndex = 13;
             dealer5.TabStop = false;
@@ -227,9 +241,10 @@
             // 
             player6.BackColor = Color.Transparent;
             player6.BackgroundImageLayout = ImageLayout.None;
-            player6.Location = new Point(637, 238);
+            player6.Location = new Point(728, 317);
+            player6.Margin = new Padding(3, 4, 3, 4);
             player6.Name = "player6";
-            player6.Size = new Size(105, 147);
+            player6.Size = new Size(120, 196);
             player6.SizeMode = PictureBoxSizeMode.StretchImage;
             player6.TabIndex = 16;
             player6.TabStop = false;
@@ -238,18 +253,20 @@
             // 
             dealer6.BackColor = Color.Transparent;
             dealer6.BackgroundImageLayout = ImageLayout.None;
-            dealer6.Location = new Point(637, 52);
+            dealer6.Location = new Point(728, 69);
+            dealer6.Margin = new Padding(3, 4, 3, 4);
             dealer6.Name = "dealer6";
-            dealer6.Size = new Size(105, 147);
+            dealer6.Size = new Size(120, 196);
             dealer6.SizeMode = PictureBoxSizeMode.StretchImage;
             dealer6.TabIndex = 15;
             dealer6.TabStop = false;
             // 
             // deal
             // 
-            deal.Location = new Point(359, 551);
+            deal.Location = new Point(410, 735);
+            deal.Margin = new Padding(3, 4, 3, 4);
             deal.Name = "deal";
-            deal.Size = new Size(50, 23);
+            deal.Size = new Size(57, 31);
             deal.TabIndex = 17;
             deal.Text = "Deal";
             deal.UseVisualStyleBackColor = true;
@@ -258,19 +275,19 @@
             // label_chips
             // 
             label_chips.AutoSize = true;
-            label_chips.Location = new Point(359, 463);
+            label_chips.Location = new Point(410, 617);
             label_chips.Name = "label_chips";
-            label_chips.Size = new Size(86, 15);
+            label_chips.Size = new Size(107, 20);
             label_chips.TabIndex = 18;
             label_chips.Text = "Your chips: 100";
             label_chips.Click += label2_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(831, 637);
+            ClientSize = new Size(950, 849);
             Controls.Add(label_chips);
             Controls.Add(deal);
             Controls.Add(player6);
@@ -290,6 +307,7 @@
             Controls.Add(label_value);
             Controls.Add(stand_button);
             Controls.Add(hit_button);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dealer1).EndInit();
